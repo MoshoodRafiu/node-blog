14d96973867f03894279bb5233de299e9959d860
devKunle <meezyloaded@gmail.com>
2021-06-19 09:18:29 +0100

    add route and structure folder

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/', (req, res) => {
     console.log(req);
 });
