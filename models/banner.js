d59a73287e90e4fedde2ca90c4aa27a2c8b89f21
devKunle <meezyloaded@gmail.com>
2021-06-19 11:52:43 +0100

    setup user model

---
+++ models/banner.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../utils/database');
+
+const User = sequelize.define('user', {
+    id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    }
+});
--- models/user.js
+++ models/user.js
@@ -5,6 +5,20 @@ const User = sequelize.define('user', {
     id: {
         type: Sequelize.INTEGER,
         allowNull: false,
-        autoIncrement: 
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
     }
 });
