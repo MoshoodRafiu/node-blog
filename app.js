0d1aac929e7d8ea3e23d6eb168ff08a60128f0c3
devKunle <meezyloaded@gmail.com>
2021-06-20 09:18:33 +0100

    add users and categories database seeder

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes/web');
 const association = require('./utils/association');
+const seeder = require('./utils/seeder');
 
 const app = express();
 
@@ -13,8 +14,11 @@ app.set('views', 'views');
 
 app.use(routes);
 
-association.syncDBWithAssociations(false)
-    .then(result => console.log(result))
+association.syncDBWithAssociations(true)
+    .then(result => {
+        console.log(result)
+        seeder.run();
+    })
     .catch(err => console.log(err));
 
 app.listen(3000);
---
+++ utils/seeder.js
@@ -0,0 +1,32 @@
+const faker = require('faker');
+const Category = require('../models/category');
+const User = require('../models/user');
+
+const seedUsers = async (val) => {
+    let users = [];
+    for(let i = 0; i < val; i++){
+        users.push({
+            name: faker.name.findName(),
+            email: faker.internet.email(),
+            password: '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
+        });
+    }
+    User.bulkCreate(users);
+}
+
+const seedCategories = async (val) => {
+    let categories = [];
+    for(let i = 0; i < val; i++){
+        categories.push({
+            name: faker.random.word(),
+            slug: faker.lorem.slug(),
+            description: faker.lorem.paragraph()
+        });
+    }
+    Category.bulkCreate(categories);
+}
+
+exports.run = async function(){
+    await seedUsers(10);
+    await seedCategories(10);
+}
